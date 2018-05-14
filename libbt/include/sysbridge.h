602b47c0a52742c81023c3bbe4e910948ce2f2a0
Xindong Xu <xindong.xu@amlogic.com>
2018-05-14 10:05:54 +0800

    amlogic: rm libbt [1/1]
    PD# NONE
    
    rm libbt
    
    Change-Id: Ic97948cb176578bbb862fabf934d16ae6fb218dd

--- libbt/include/sysbridge.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYSBRIDGE_H
-#define SYSBRIDGE_H
-
-
-int amSystemWriteSetProperty(const char* key, const char* value, int leth);
-int amSystemWriteGetProperty(const char* key, char* value);
-
-#endif
