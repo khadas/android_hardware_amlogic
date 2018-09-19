e1bed9d9f82edf6c6163db4730e6b910be47d4dc
Lu Wang <lu.wang@amlogic.com>
2018-09-19 13:24:25 +0800

    keymaster/Android.mk [1/1]
    PD# 172049
    
    TA needs to be signed in order to run on secure boot enabled platforms
    
    Change-Id: If822bd3dbc96ab25a2daeb5d0b376ecd812249bd

--- keymaster/Android.mk
+++ keymaster/Android.mk
@@ -74,7 +74,8 @@ ifeq ($(TARGET_ENABLE_TA_SIGN), true)
 $(info $(shell mkdir -p $(shell pwd)/$(PRODUCT_OUT)/signed/keymaster))
 $(info $(shell $(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/scripts/sign_ta_auto.py \
 		--in=$(shell pwd)/$(LOCAL_PATH)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX) \
-		--out=$(shell pwd)/$(PRODUCT_OUT)/signed/keymaster/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)))
+		--out=$(shell pwd)/$(PRODUCT_OUT)/signed/keymaster/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX) \
+		--keydir=$(shell pwd)/$(BOARD_AML_TDK_KEY_PATH)))
 LOCAL_SRC_FILES := ../../../$(PRODUCT_OUT)/signed/keymaster/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
 else
 LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
