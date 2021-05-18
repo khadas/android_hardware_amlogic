d88dd48b44f4b8a65d28e2c9f9cbb7f99a20f8d9
sijia.lin <sijia.lin@amlogic.com>
2021-05-18 09:29:15 +0800

    audio: increase dtshd buffer size for DTS VBR stream [1/1]
    PD#SWPL-48445
    
    Problem:
      Some DTS bitstream have larger framesize,
      but the decoded PCM is relatively small.
      Such as E26 and E27 are variable bit rates,
      sometimes as high as 22376 bytes per frame,
      but only produce 10.6ms of PCM.
      This leads to underrun randomly.
    
    Solution:
      increase the dtshd es buffer size to 32768.
    
    Verify:
      ANR, T5D
      Exoplayer, DTS E26/E27
    
    Change-Id: I83a734e604d2f94bd59c9c802e95bb0829d5f698
    Signed-off-by: sijia.lin <sijia.lin@amlogic.com>

--- audio/audio_hal/audio_hw.c
+++ audio/audio_hal/audio_hw.c
@@ -785,7 +785,7 @@ static size_t out_get_buffer_size (const struct audio_stream *stream)
         if (stream->get_format(stream) == AUDIO_FORMAT_IEC61937) {
             size = 4 * PLAYBACK_PERIOD_COUNT * DEFAULT_PLAYBACK_PERIOD_SIZE;
         } else {
-            size = DTSHD_PERIOD_SIZE * 4;
+            size = DTSHD_PERIOD_SIZE * 8;
         }
         ALOGI("%s AUDIO_FORMAT_DTS_HD buffer size = %zuframes", __FUNCTION__, size);
         break;
