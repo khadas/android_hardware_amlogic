930bcdcac04d56dc09c7ec61e203cda2f6539cad
chuntian.miao <chuntian.miao@amlogic.com>
2021-05-25 20:42:12 +0800

    Audio: adjust latency for TV AVsync MS12 [1/1]
    PD#SWPL-53797
    
    Problem:
    SYNC-AV1-23FPS-DDP51 case has a right offset.
    
    Solution:
    adjust tunning latency for these cases.
    
    Verify:
    T950X4.
    
    Signed-off-by: chuntian.miao <chuntian.miao@amlogic.com>
    Change-Id: I13f65212e2b43fdd6e1132fd1e559f0a3c776744

--- audio/audio_hal/aml_audio_ms12_sync.c
+++ audio/audio_hal/aml_audio_ms12_sync.c
@@ -289,6 +289,48 @@ static int get_ms12_netflix_output_latency(audio_format_t output_format) {
 
 }
 
+int get_ms12_netflix_port_latency( enum OUT_PORT port, audio_format_t output_format)
+{
+    int latency_ms = 0;
+    int ret = 0;
+    char *prop_name = NULL;
+    char buf[PROPERTY_VALUE_MAX];
+
+    switch (port)  {
+        case OUTPORT_HDMI_ARC:
+            if (output_format == AUDIO_FORMAT_AC3)
+                latency_ms = AVSYNC_MS12_HDMI_ARC_OUT_DD_LATENCY;
+            else if (output_format == AUDIO_FORMAT_E_AC3)
+                latency_ms = AVSYNC_MS12_HDMI_ARC_OUT_DDP_LATENCY;
+            else
+                latency_ms = AVSYNC_MS12_HDMI_ARC_OUT_PCM_LATENCY;
+
+            prop_name = AVSYNC_MS12_NETFLIX_HDMI_ARC_OUT_LATENCY_PROPERTY;
+            break;
+        case OUTPORT_HDMI:
+            //latency_ms = AVSYNC_MS12_HDMI_OUT_LATENCY;
+            //prop_name = AVSYNC_MS12_NETFLIX_HDMI_LATENCY_PROPERTY;
+            break;
+        case OUTPORT_SPEAKER:
+        case OUTPORT_AUX_LINE:
+            latency_ms = AVSYNC_MS12_HDMI_SPEAKER_LATENCY;
+            prop_name = AVSYNC_MS12_NETFLIX_HDMI_SPEAKER_LATENCY_PROPERTY;
+            break;
+        default :
+            break;
+    }
+
+    if (prop_name) {
+        ret = property_get(prop_name, buf, NULL);
+        if (ret > 0) {
+            latency_ms = atoi(buf);
+        }
+    }
+    ALOGV("%s output format =0x%x latency ms =%d", __func__, output_format, latency_ms);
+
+    return latency_ms;
+}
+
 int get_ms12_port_latency( enum OUT_PORT port, audio_format_t output_format)
 {
     int latency_ms = 0;
@@ -347,12 +389,15 @@ static int get_ms12_tunnel_latency_offset(enum OUT_PORT port
     int port_latency_ms = 0;
     int is_dv = getprop_bool(MS12_OUTPUT_5_1_DDP); /* suppose that Dolby Vision is under test */
 
+    //ALOGD("%s  prot:%d, is_netflix:%d, input_format:0x%x, output_format:0x%x", __func__,
+    //            port, is_netflix, input_format, output_format);
     if (is_netflix) {
         input_latency_ms  = get_ms12_netflix_tunnel_input_latency(input_format);
         output_latency_ms = get_ms12_netflix_output_latency(output_format);
         if ((output_format == AUDIO_FORMAT_E_AC3) || (output_format == AUDIO_FORMAT_AC3)) {
             output_latency_ms += AVSYNC_MS12_NETFLIX_DDP_OUT_TUNNEL_TUNNING;
         }
+        port_latency_ms   = get_ms12_netflix_port_latency(port, output_format);
     } else {
         /*
          * TODO:
