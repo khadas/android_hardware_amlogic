f3c7e284b7c3f36cd8d12de78fe969755c93fe2c
xiaoming sui <xiaoming.sui@amlogic.com>
2021-07-02 14:35:12 +0800

    audiohal: add prop for dtv audio fadeout time [1/1]
    PD#SWPL-46427
    
    Problem:
    quickly audio replayed many times, timeout for last playback;
    
    Solution:
    1, add prop for dtv audio fadeout time;
    2, default tv fadeout time to 40ms;
    
    Verify:
    verified by T5D-R
    
    Signed-off-by: xiaoming sui <xiaoming.sui@amlogic.com>
    Change-Id: Ifa5eec9ba1aa9371c3c3b27a5d5ea340eadc46db

--- audio/audio_hal/audio_hw_dtv.c
+++ audio/audio_hal/audio_hw_dtv.c
@@ -122,11 +122,15 @@ static void dtv_do_ease_out(struct aml_audio_device *aml_dev)
 {
     if (aml_dev && aml_dev->audio_ease) {
         ALOGI("%s(), do fade out", __func__);
-        start_ease_out(aml_dev->audio_ease, aml_dev->is_TV);
-        if (aml_dev->is_TV)
-            usleep(AUDIO_FADEOUT_TV_SLEEP_US);
-        else
+        if (aml_dev->is_TV) {
+            int duration_ms;
+            duration_ms = property_get_int32("vendor.media.audio.dtv.fadeout.us", AUDIO_FADEOUT_TV_DURATION_US) / 1000;
+            start_ease_out(aml_dev->audio_ease, aml_dev->is_TV, duration_ms - 10);
+            usleep(duration_ms * 1000);
+        } else {
+            start_ease_out(aml_dev->audio_ease, aml_dev->is_TV, 0);
             usleep(AUDIO_FADEOUT_STB_SLEEP_US);
+        }
     }
 }
 
