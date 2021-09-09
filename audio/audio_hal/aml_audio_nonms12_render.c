4e084ba7d5a1eb7a4461fb04e9bb9bfb7ed0a7c6
Lianlian Zhu <lianlian.zhu@amlogic.com>
2021-09-09 09:24:21 +0800

    audio: fixed netflix switch to dtv crash issue [1/1]
    PD#SWPL-58554
    
    Problem:
    {Android R+DTVkit+SC2}play Netflix video,switch to
    TV,video freeze (crash),Frequency:2/10,Comparison:none
    
    Solution:
    when neflix switch to dtv quickly, two output streams
    coexist. so need to distinguish tv path behavior from
    audiotrack behavior.
    
    Verify:
    T3
    
    Signed-off-by: Lianlian Zhu <lianlian.zhu@amlogic.com>
    Change-Id: I34d84f294b8b6dd184378c74d01e9eff16bdcea2

--- audio/audio_hal/aml_audio_nonms12_render.c
+++ audio/audio_hal/aml_audio_nonms12_render.c
@@ -120,7 +120,7 @@ int aml_audio_nonms12_render(struct audio_stream_out *stream, const void *buffer
     struct aml_audio_device *adev = aml_out->dev;
     struct aml_audio_patch *patch = adev->audio_patch;
     struct aml_native_postprocess *VX_postprocess = &adev->native_postprocess;
-    bool do_sync_flag = adev->patch_src  == SRC_DTV && patch && patch->skip_amadec_flag;
+
     int return_bytes = bytes;
     int out_frames = 0;
     void *input_buffer = (void *)buffer;
@@ -132,6 +132,9 @@ int aml_audio_nonms12_render(struct audio_stream_out *stream, const void *buffer
     int decoder_remain_size = 0;
     dtvsync_process_res process_result = DTVSYNC_AUDIO_OUTPUT;
 
+    bool do_sync_flag = adev->patch_src  == SRC_DTV && patch && patch->skip_amadec_flag && aml_out->tv_src_stream;
+    bool dtv_stream_flag = patch && (adev->patch_src  == SRC_DTV) && aml_out->tv_src_stream;
+
     if (aml_out->aml_dec == NULL) {
         config_output(stream, true);
 
@@ -139,7 +142,7 @@ int aml_audio_nonms12_render(struct audio_stream_out *stream, const void *buffer
 
     aml_dec_t *aml_dec = aml_out->aml_dec;
 
-    if (patch && adev->patch_src ==  SRC_DTV && patch->decoder_offset == 0) {
+    if (dtv_stream_flag  && patch->decoder_offset == 0) {
         if (patch->cur_package) {
             aml_dec->first_in_frame_pts = patch->cur_package->pts;
         } else {
@@ -215,7 +218,7 @@ int aml_audio_nonms12_render(struct audio_stream_out *stream, const void *buffer
                     decoder_latency = DDP_DECODER_CACHE + decoder_remain_cache;
                 }
 
-                if (aml_dec->debug_synced_frame_pts_flag) {
+                if (do_sync_flag && aml_dec->debug_synced_frame_pts_flag) {
                     int pre_zero_samples = 0;
                     bool is_beep_frame = check_beep_frame(dec_pcm_data->buf, dec_pcm_data->data_len, &pre_zero_samples);
                     if (is_beep_frame)  {
@@ -235,7 +238,7 @@ int aml_audio_nonms12_render(struct audio_stream_out *stream, const void *buffer
                 audio_format_t output_format = AUDIO_FORMAT_PCM_16_BIT;
                 void  *dec_data = (void *)dec_pcm_data->buf;
                 int pcm_len = dec_pcm_data->data_len;
-                if (patch && adev->patch_src  == SRC_DTV &&
+                if (do_sync_flag &&
                     (adev->start_mute_flag == 1 || adev->tv_mute)) {
                     memset(dec_pcm_data->buf, 0, dec_pcm_data->data_len);
                 }
@@ -295,7 +298,7 @@ int aml_audio_nonms12_render(struct audio_stream_out *stream, const void *buffer
 
                     }
                     //sync process here
-                    if (adev->patch_src  == SRC_DTV && aml_out->dtvsync_enable) {
+                    if (do_sync_flag && aml_out->dtvsync_enable) {
 
                         process_result = aml_dtvsync_nonms12_process(stream, duration, &speed_enabled);
                         if (process_result == DTVSYNC_AUDIO_DROP)
@@ -371,7 +374,7 @@ int aml_audio_nonms12_render(struct audio_stream_out *stream, const void *buffer
         } while ((left_bytes > 0) || aml_dec->fragment_left_size || try_again);
     }
 
-    if (patch && (adev->patch_src  == SRC_DTV)) {
+    if (dtv_stream_flag) {
         aml_demux_audiopara_t *demux_info = (aml_demux_audiopara_t *)patch->demux_info;
         if (demux_info && demux_info->dual_decoder_support == 0)
             patch->decoder_offset +=return_bytes;
