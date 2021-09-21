d2a98c0cc8ab435051a7589a37f8bcd79b4e8f9b
chuntian.miao <chuntian.miao@amlogic.com>
2021-09-21 10:58:53 +0800

    Audio: adjust AV sync for Y4-S4D [1/1]
    PD#SWPL-59968
    
    Problem:
    AV sync is out of normal range at DV cases.
    
    Solution:
    adjust netflix tunnel input latency.
    
    Verify:
    S905Y4-S4D
    
    Signed-off-by: chuntian.miao <chuntian.miao@amlogic.com>
    Change-Id: Iea906d813800b465cc0144db4198c5a3b4f48242

--- audio/audio_hal/aml_audio_avsync_table.h
+++ audio/audio_hal/aml_audio_avsync_table.h
@@ -107,15 +107,15 @@
 
 /*netflix tunning part*/
 // right offset. 10-->30
-#define  AVSYNC_MS12_NETFLIX_NONTUNNEL_PCM_LATENCY       (30)
+#define  AVSYNC_MS12_NETFLIX_NONTUNNEL_PCM_LATENCY       (20)
 // right offset. 20-->40
 #define  AVSYNC_MS12_NETFLIX_NONTUNNEL_DDP_LATENCY       (40)
 #define  AVSYNC_MS12_NETFLIX_NONTUNNEL_ATMOS_LATENCY     (-18) /*for atmos we remove 32ms at the beginning*/
 // right offset. -10-->20
-#define  AVSYNC_MS12_NETFLIX_TUNNEL_PCM_LATENCY          (20)
+#define  AVSYNC_MS12_NETFLIX_TUNNEL_PCM_LATENCY          (10)
 // right offset. 65-->95-->75
 #define  AVSYNC_MS12_NETFLIX_TUNNEL_DDP_LATENCY          (75)
-#define  AVSYNC_MS12_NETFLIX_TUNNEL_ATMOS_LATENCY        (5)
+#define  AVSYNC_MS12_NETFLIX_TUNNEL_ATMOS_LATENCY        (-5)
 
 #define  AVSYNC_MS12_NETFLIX_PCM_OUT_LATENCY             (0)
 #define  AVSYNC_MS12_NETFLIX_DD_OUT_LATENCY              (0)
