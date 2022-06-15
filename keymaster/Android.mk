b678f135610d15b189eaf6b7b790bc4a09290e26
yidong zhang <yidong.zhang@amlogic.com>
2022-06-15 15:21:13 +0800

    [LSC] Add LOCAL_LICENSE_KINDS to hardware/amlogic
    Added SPDX-license-identifier-Apache-2.0 to:
      keymaster/Android.mk
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    Change-Id: I2375d4ac256efb7d54c4e3b9bd2c89a871cd4705

--- keymaster/Android.mk
+++ keymaster/Android.mk
@@ -52,6 +52,9 @@ LOCAL_SRC_FILES := android.hardware.hardware_keystore.amlogic.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/permissions
 LOCAL_MODULE_TAGS := optional
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
+LOCAL_NOTICE_FILE := $(LOCAL_PATH)/../LICENSE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
