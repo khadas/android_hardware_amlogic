7de48ad19a8ebcdde22d1253fb25dba56f3f9fb5
zehong.luo <zehong.luo@amlogic.com>
2022-07-29 10:33:47 +0800

    media: Sync the android S esplayer with android R [1/1]
    PD#SWPL-84440
    
    Problem:
    The esplayer in S is not synced with R
    
    Solution:
    Sync the android S esplayer with android R
    
    Verify:
    SC2
    
    Change-Id: I2da073786c170db63d72eae7efa9f5383df9c6f8
    Signed-off-by: zehong.luo <zehong.luo@amlogic.com>

--- media/player/Android.mk
+++ media/player/Android.mk
@@ -2,32 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := esplayer
-LOCAL_LICENSE_KINDS := legacy_by_exception_only legacy_proprietary
-LOCAL_LICENSE_CONDITIONS := by_exception_only proprietary
-LOCAL_NOTICE_FILE := $(LOCAL_PATH)/../LICENSE
 LOCAL_MODULE_TAGS := optional
-LOCAL_ARM_MODE := arm
+#LOCAL_ARM_MODE := arm
 LOCAL_SRC_FILES := \
 	esplayer.c \
 	vcodec.c
-
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/../amcodec/include
-
-LOCAL_LDLIBS := -llog
-LOCAL_VENDOR_MODULE := true
-
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE    := DecInfo_test
-LOCAL_LICENSE_KINDS := legacy_by_exception_only legacy_proprietary
-LOCAL_LICENSE_CONDITIONS := by_exception_only proprietary
-LOCAL_NOTICE_FILE := $(LOCAL_PATH)/../LICENSE
-LOCAL_MODULE_TAGS := optional
-LOCAL_ARM_MODE := arm
-LOCAL_SRC_FILES := \
-        DecInfo_test.c
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/../amcodec/include \
@@ -35,5 +14,41 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_LDLIBS := -llog
 LOCAL_VENDOR_MODULE := true
-
+LOCAL_CFLAGS += -Wno-unused-label -Wno-unused-parameter -Wno-format -Wno-switch
 include $(BUILD_EXECUTABLE)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := dec_slt
+LOCAL_MODULE_TAGS := optional
+#LOCAL_ARM_MODE := arm
+LOCAL_SRC_FILES := \
+	dec_slt.c \
+	vcodec.c
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/../amcodec/include \
+	$(JNI_H_INCLUDE)
+
+LOCAL_LDLIBS := -llog
+LOCAL_VENDOR_MODULE := true
+LOCAL_CFLAGS += -Wno-unused-label -Wno-unused-parameter -Wno-format -Wno-switch
+include $(BUILD_EXECUTABLE)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := DecInfo_test
+LOCAL_MODULE_TAGS := optional
+#LOCAL_ARM_MODE := arm
+LOCAL_SRC_FILES := \
+	DecInfo_test.c
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/../amcodec/include \
+	$(JNI_H_INCLUDE)
+
+LOCAL_LDLIBS := -llog
+LOCAL_VENDOR_MODULE := true
+LOCAL_CFLAGS += -Wno-unused-label -Wno-unused-parameter -Wno-format -Wno-switch
+include $(BUILD_EXECUTABLE)
+
