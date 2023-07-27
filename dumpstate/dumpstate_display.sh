05247dccadbf2eefa26a864c86b75128b4f3733b
zhenteng.tian <zhenteng.tian@amlogic.com>
2023-07-27 14:48:43 +0800

    dumpstate: display bugreport debug integrate [1/1]
    PD#SWPL-131794
    
    Problem:
    display bugreport debug integrate
    
    Solution:
    add debug command in dumpstate_display.sh
    
    Verify:
    ohm
    
    Change-Id: I4909a3d71088fb2a6353c1b05dc455946cab9003
    Signed-off-by: zhenteng.tian <zhenteng.tian@amlogic.com>

--- dumpstate/dumpstate_display.sh
+++ dumpstate/dumpstate_display.sh
@@ -1,92 +1,135 @@
+set -x
 echo "################Start dumpstate display################"
-echo "Video Frame information"
-cat /sys/class/video/vframe_states
-cat /sys/class/ppmgr/ppmgr_vframe_states
-cat /sys/class/ionvideo/vframe_states
-cat /sys/class/vfm/map
-
-echo "osd registers informations"
-echo dump > /sys/devices/platform/fb/graphics/fb0/debug
-cat /sys/devices/platform/fb/graphics/fb0/window_axis
-cat /sys/devices/platform/fb/graphics/fb0/scale_width
-cat /sys/devices/platform/fb/graphics/fb0/scale_height
-
-echo "dump video information"
-echo 1 > /sys/module/amvideo/parameters/debug_flag
-cat /sys/class/video/video_state
-cat /sys/class/video/frame_width
-cat /sys/class/video/frame_height
-cat /sys/class/video/axis
-cat /sys/class/video/crop
-cat /sys/class/video/screen_mode
-cat /sys/class/video/frame_rate
-
-echo "dump amvideo information"
-cat /sys/module/amvideo/parameters/process_3d_type
-cat /sys/module/amvideo/parameters/sharpness1_sr2_ctrl_32d7
-cat /sys/module/amvideo/parameters/sharpness1_sr2_ctrl_3280
+echo "dump DV information"
+cat /sys/class/amhdmitx/amhdmitx0/dv_cap
+cat /sys/class/amhdmitx/amhdmitx0/hdr_cap
+echo "cat /sys/class/amhdmitx/amhdmitx0/attr"
+cat /sys/class/amhdmitx/amhdmitx0/attr
+echo "cat /sys/class/amdolby_vision/dv_mode"
+cat /sys/class/amdolby_vision/dv_mode
+echo "cat /sys/class/amvecm/hdr_dbg"
+cat /sys/class/amvecm/hdr_dbg
+echo "cat /sys/module/aml_media/parameters/hdr_policy"
+cat /sys/module/aml_media/parameters/hdr_policy
+echo "cat /sys/module/aml_media/parameters/force_output"
+cat /sys/module/aml_media/parameters/force_output
+echo "cat /sys/class/amhdmitx/amhdmitx0/hdmi_hdr_status"
+cat /sys/class/amhdmitx/amhdmitx0/hdmi_hdr_status
+echo "cat /sys/module/aml_media/parameters/dolby_vision_policy"
+cat /sys/module/aml_media/parameters/dolby_vision_policy
+echo "cat /sys/module/aml_media/parameters/dolby_vision_ll_policy"
+cat /sys/module/aml_media/parameters/dolby_vision_ll_policy
+echo "cat /sys/class/amdolby_vision/src_format"
+cat /sys/class/amdolby_vision/src_format
+echo "cat /sys/module/aml_media/parameters/dolby_vision_enable"
+cat /sys/module/aml_media/parameters/dolby_vision_enable
 
 echo "dump DI information"
-echo dumpreg > /sys/class/deinterlace/di0/debug
-echo state > /sys/class/deinterlace/di0/debug
-cat /sys/module/di/parameters/di_vscale_skip_count_real
-cat /sys/class/deinterlace/di0/provider_vframe_status
+#mount -t debugfs none /sys/kernel/debug/
+echo "/cat sys/kernel/debug/di*/state"
+cat /sys/kernel/debug/di1/state
+cat /sys/kernel/debug/di2/state
+cat /sys/kernel/debug/di3/state
+echo "cat /sys/kernel/debug/di*/list_ndis"
+cat /sys/kernel/debug/di1/list_ndis
+cat /sys/kernel/debug/di2/list_ndis
+cat /sys/kernel/debug/di3/list_ndis
+echo "cat di_top policy"
+cat /sys/kernel/debug/di_top/policy
+echo "cat /sys/kernel/debug/di*/rvfm_out"
+cat /sys/kernel/debug/di1/rvfm_out
+cat /sys/kernel/debug/di2/rvfm_out
+cat /sys/kernel/debug/di3/rvfm_out
+echo "/sys/kernel/debug/di*/rvfm_in"
+cat /sys/kernel/debug/di1/rvfm_in
+cat /sys/kernel/debug/di2/rvfm_in
+cat /sys/kernel/debug/di3/rvfm_in
+
+echo "dump VPP information"
+echo "cat /sys/class/vfm/map"
+cat /sys/class/vfm/map
+echo "/sys/class/video/frame_width"
+cat /sys/class/video/frame_width
+echo "/sys/class/video/frame_height"
+cat /sys/class/video/frame_height
+echo "cat /sys/class/video/axis"
+cat /sys/class/video/axis
+echo "cat /sys/class/video/crop"
+cat /sys/class/video/crop
+echo "cat /sys/class/video/video_state"
 cat /sys/class/video/video_state
-cat /sys/class/video/vframe_states
-cat /sys/class/amvecm/dump_reg
 
-echo "dump displaymode information"
-cat /sys/class/display/mode
-cat /sys/class/amhdmitx/amhdmitx0/disp_mode
-cat /sys/class/amhdmitx/amhdmitx0/attr
-cat /sys/class/amhdmitx/amhdmitx0/rawedid
-cat /sys/class/amhdmitx/amhdmitx0/disp_cap
-cat /sys/class/amhdmitx/amhdmitx0/dc_cap
-cat /sys/class/amhdmitx/amhdmitx0/aud_cap
-cat /sys/class/amhdmitx/amhdmitx0/hdr_cap
-cat /sys/class/amhdmitx/amhdmitx0/hdr_cap2
-cat /sys/class/amhdmitx/amhdmitx0/dv_cap
-cat /sys/class/amhdmitx/amhdmitx0/dv_cap2
-cat /sys/class/amhdmitx/amhdmitx0/allm_cap
-cat /sys/class/amhdmitx/amhdmitx0/contenttype_cap
-cat /sys/class/amhdmitx/amhdmitx0/hpd_state
-cat /sys/class/amhdmitx/amhdmitx0/hdcp_lstore
-cat /sys/class/amhdmitx/amhdmitx0/hdcp_mode
-cat /sys/class/amhdmitx/amhdmitx0/cedst_policy
-
-echo "dump hdmi registers"
-cat /sys/kernel/debug/amhdmitx/aud_cts
-cat /sys/kernel/debug/amhdmitx/bus_reg
-cat /sys/kernel/debug/amhdmitx/hdmi_pkt
-cat /sys/kernel/debug/amhdmitx/hdmi_reg
-cat /sys/kernel/debug/amhdmitx/hdmi_timing
-echo state > /sys/class/hdmirx/hdmirx0/debug
+echo "dump vdin information"
+echo "cat /sys/class/hdmirx/hdmirx0/info"
 cat /sys/class/hdmirx/hdmirx0/info
+echo "echo state > /sys/class/hdmirx/hdmirx0/debug"
+echo state > /sys/class/hdmirx/hdmirx0/debug
+echo "echo state2 > /sys/class/hdmirx/hdmirx0/debug"
+echo state2 > /sys/class/hdmirx/hdmirx0/debug
+echo "echo state > /sys/class/vdin/vdin0/attr"
+echo state > /sys/class/vdin/vdin0/attr
+echo "echo fps > /sys/class/vdin/vdin0/attr"
+echo fps > /sys/class/vdin/vdin0/attr
+echo "echo counter > /sys/class/vdin/vdin0/attr"
+echo counter > /sys/class/vdin/vdin0/attr
+echo "echo histgram > /sys/class/vdin/vdin0/attr"
+echo histgram > /sys/class/vdin/vdin0/attr
+echo "echo state > /sys/class/vdin/vdin0/sct_attr"
+echo state > /sys/class/vdin/vdin0/sct_attr
+echo "echo state > /sys/class/vdin/vdin1/attr"
+echo state > /sys/class/vdin/vdin1/attr
+echo "cat /sys/class/vdin/vdin0/vf_log"
+cat /sys/class/vdin/vdin0/vf_log
+echo "cat /sys/class/vdin/vdin1/vf_log"
+cat /sys/class/vdin/vdin1/vf_log
+echo "echo start > /sys/class/vdin/vdin0/vf_log"
+echo start > /sys/class/vdin/vdin0/vf_log
+echo "echo start > /sys/class/vdin/vdin1/vf_log"
+echo start > /sys/class/vdin/vdin1/vf_log
+sleep 1;
+echo "echo print > /sys/class/vdin/vdin0/vf_log"
+echo print > /sys/class/vdin/vdin0/vf_log
+echo "echo print > /sys/class/vdin/vdin1/vf_log"
+echo print > /sys/class/vdin/vdin1/vf_log
 
-echo "dump video panel"
-echo dump > /sys/class/lcd/debug
+echo "dump FRC information"
+echo status > /sys/class/frc/debug
 
-echo "dump backlight information"
-cat /sys/class/aml_bl/status
-cat /sys/class/aml_bl/pwm
-
-echo "dump vout information"
-cat /sys/class/display/vinfo
-
-echo "dump vout information"
-echo state >/sys/devices/platform/vdin0/vdin/vdin0/attr
-echo state >/sys/devices/platform/vdin1/vdin/vdin1/attr
-echo start > /sys/devices/platform/vdin0/vdin/vdin0/vf_log
-sleep 2;
-echo print > /sys/devices/platform/vdin0/vdin/vdin0/vf_log
-echo dump_reg > /sys/devices/platform/vdin0/vdin/vdin0/attr
-echo dump_reg > /sys/devices/platform/vdin1/vdin/vdin1/attr
-
-echo "dump tvafe information"
-echo D > /sys/class/tvafe/tvafe0/reg
-
-echo "dump amvecm information"
-cat /sys/class/amvecm/hdr_dbg
-cat /sys/class/amvecm/hdr_reg
-cat /sys/module/am_vecm/parameters/cur_csc_type
+echo "dump video_pipeline information"
+echo "cat /sys/class/video/video_global_output"
+cat /sys/class/video/video_global_output
+echo "cat /sys/class/video/disable_video"
+cat /sys/class/video/disable_video
+echo "cat /sys/module/aml_media/parameters/video_mute_on"
+cat /sys/module/aml_media/parameters/video_mute_on
+echo "cat /sys/module/aml_media/parameters/new_frame_count"
+cat /sys/module/aml_media/parameters/new_frame_count
+echo "cat /sys/class/video/hold_video"
+cat /sys/class/video/hold_video
+echo "cat /sys/class/video/path_select"
+cat /sys/class/video/path_select
+echo "cat /sys/class/video/video_state"
+cat /sys/class/video/video_state
+echo "echo dump all > /sys/class/vfm/map"
+echo dump all > /sys/class/vfm/map
+echo "echo rv 1dfb > /sys/class/amvecm/reg"
+echo rv 1dfb > /sys/class/amvecm/reg
+echo "echo rv 1de1 > /sys/class/amvecm/reg"
+echo rv 1de1 > /sys/class/amvecm/reg
+echo "echo rv 1de2 > /sys/class/amvecm/reg"
+echo rv 1de2 > /sys/class/amvecm/reg
+cat /sys/class/v4lvideo/dec_count
+cat /sys/class/v4lvideo/dq_count
+cat /sys/class/v4lvideo/get_count
+cat /sys/class/v4lvideo/link_fd_count
+cat /sys/class/v4lvideo/link_put_fd_count
+cat /sys/class/v4lvideo/open_fd_count
+cat /sys/class/v4lvideo/put_count
+cat /sys/class/v4lvideo/q_count
+cat /sys/class/v4lvideo/release_fd_count
+cat /sys/class/v4lvideo/total_get_count
+cat /sys/class/v4lvideo/total_put_count
+cat /sys/class/v4lvideo/total_release_count
+echo "cat /sys/class/vdec/dump_decoder_state"
+cat /sys/class/vdec/dump_decoder_state
 echo "################End dumpstate display################"
