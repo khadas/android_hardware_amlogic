22ffa9407d3fc3a4861ffb85c2d219f2c16c2b42
shuanshuan.jiang <shuanshuan.jiang@amlogic.com>
2023-11-20 15:07:13 +0800

    camera: fix issue of preview mosaic [1/1]
    PD#SWPL-146049
    
    Problem:
    preview is mosaic in bluebell board
    
    Solution:
    add checkout in camera hal
    
    Verify:
    local
    
    Change-Id: Ie88d9d67c07bdc9bca26da3b8ca9b801fdc7035d
    Signed-off-by: shuanshuan.jiang <shuanshuan.jiang@amlogic.com>

--- camera/v3/Android.mk
+++ camera/v3/Android.mk
@@ -33,6 +33,8 @@ ifeq ($(TARGET_PRODUCT), t7_an400)
 DEWARP_ENABLE := true
 else ifeq ($(TARGET_PRODUCT), t7_an400_arm64)
 DEWARP_ENABLE := true
+else ifeq ($(TARGET_PRODUCT), bluebell_arm64)
+DEWARP_ENABLE := true
 endif
 
 
