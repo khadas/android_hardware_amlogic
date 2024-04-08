d3acef87da9b527bfd5a4f04e509345cb79e556c
yang.zhou <yang.zhou@amlogic.com>
2024-04-08 14:25:47 +0800

    memtrack: solve too much memtrack logs [1/1]
    PD#SWPL-163538
    
    Problem:
    android-u txhd2 has too much memtrack logs
    
    Solution:
    when the gpu is mali450 then return
    
    Verify:
    android-u txhd2
    
    Change-Id: I132e3b6818bfdc672785bf455620a4fe8dca95b8
    Signed-off-by: yang.zhou <yang.zhou@amlogic.com>

--- memtrack/aidl_memtrack/core/GpuSysfsReader.h
+++ memtrack/aidl_memtrack/core/GpuSysfsReader.h
@@ -8,7 +8,7 @@ namespace GpuSysfsReader {
 uint64_t memtrack_get_gpuMem(int pid = 0);
 uint64_t memtrack_get_EGL_Mem(int pid = 0);
 
-
+constexpr char MALI[] = "d/mali";
 constexpr char kSysfsDevicePath[] = "/sys/class/misc/mali0/device";
 
 } // namespace GpuSysfsReader
