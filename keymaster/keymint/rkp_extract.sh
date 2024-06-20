600b128a7a08443bcdb082956248b701412b9895
hao.qi <hao.qi@amlogic.com>
2024-06-20 14:04:50 +0800

    crs: user version extract csr [2/2]
    PD#SWPL-174425
    
    Problem:
    user version extract csr
    
    Solution:
    The csr file is saved to sdcard,
    and the APK is obtained from the sdcard directory.
    
    Verify:
    s7d_bm201
    
    Change-Id: Iebc6a848604f312965c2aed712946ccf4ccb11ae
    Signed-off-by: hao.qi <hao.qi@amlogic.com>

--- keymaster/keymint/rkp_extract.sh
+++ keymaster/keymint/rkp_extract.sh
@@ -1,2 +1,10 @@
-#!/vendor/bin/sh
-rkp_factory_extraction_tool > /data/vendor/csrs/csrs.json --output_format build+csr
+#!/system/bin/sh
+
+#/vendor/bin/provision_devid_demo #The 0x43 key is not burned, so you need to execute this command first.
+
+#echo "sdcard mounting. Waiting..."
+#sleep 20  # Pause for 20 seconds and wait for the sdcard to be mounted successfully.
+
+#echo "sdcard mounted. Exiting..."
+
+/vendor/bin/rkp_factory_extraction_tool > /sdcard/csrs.json --output_format build+csr
